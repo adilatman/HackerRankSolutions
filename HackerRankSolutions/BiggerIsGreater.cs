3e135e32737cbf5be9df4f93bc0ac9e0ff2c0c77
adilatman <adilatman@gmail.com>
2023-09-16 22:38:48 +0300

    BiggerIsGreater

---
+++ HackerRankSolutions/BiggerIsGreater.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class BiggerIsGreater
+    {
+        /// <summary>
+        /// The process called "Next Permutation"
+        /// </summary>
+        /// <param name="w"></param>
+        /// <returns></returns>
+        public string biggerIsGreater(string w)
+        {
+            char[] word = w.ToCharArray();
+            if (word.Length == 1)
+            {
+                return "no answer";
+            }
+            int i = word.Length - 2;
+            while (i >= 0 && word[i] >= word[i + 1])
+            {
+                i--;
+            }
+            if (i < 0)
+            {
+                return "no answer";
+            }
+            int j = word.Length - 1;
+            while (j >= 0 && word[j] <= word[i])
+            {
+                j--;
+            }
+            if (j<=0)
+            {
+                return "no answer";
+            }
+            (word[i], word[j]) = (word[j], word[i]);
+
+            Array.Sort(word, i + 1, word.Length - i - 1);
+
+            return new string(word);
+        }
+    }
+}
--- HackerRankSolutions/Program.cs
+++ HackerRankSolutions/Program.cs
@@ -7,10 +7,13 @@ namespace HackerRankSolutions
     {
         static void Main(string[] args)
         {
-            new AlmostSorted().almostSorted(new List<int>() { 1,2,8,7,6,5,4,3,9,10});
+            new AlmostSorted().almostSorted(new List<int>() { 1, 2, 8, 7, 6, 5, 4, 3, 9, 10 });
             Console.WriteLine();
 
             new ExtraLongFactorials().extraLongFactorials(100);
+            Console.WriteLine();
+
+            Console.WriteLine(new BiggerIsGreater().biggerIsGreater("dcbb"));
             Console.WriteLine();
 
 
