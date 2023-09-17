684d7da7421876b01f6d4919c8dd168e74a205a9
adilatman <adilatman@gmail.com>
2023-09-17 12:48:33 +0300

    QueensAttackII

--- HackerRankSolutions/Program.cs
+++ HackerRankSolutions/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace HackerRankSolutions
 {
@@ -7,18 +8,48 @@ namespace HackerRankSolutions
     {
         static void Main(string[] args)
         {
-            new AlmostSorted().almostSorted(new List<int>() { 1, 2, 8, 7, 6, 5, 4, 3, 9, 10 });
-            Console.WriteLine();
+            //new AlmostSorted().almostSorted(new List<int>() { 1, 2, 8, 7, 6, 5, 4, 3, 9, 10 });
+            //Console.WriteLine();
 
-            new ExtraLongFactorials().extraLongFactorials(100);
-            Console.WriteLine();
+            //new ExtraLongFactorials().extraLongFactorials(100);
+            //Console.WriteLine();
 
-            Console.WriteLine(new BiggerIsGreater().biggerIsGreater("dcbb"));
-            Console.WriteLine();
-
+            //Console.WriteLine(new BiggerIsGreater().biggerIsGreater("dcbb"));
+            //Console.WriteLine();
+            timeConversion("12:00:00AM");
 
 
             Console.ReadLine();
+        }
+        public static string timeConversion(string s)
+        {
+            int hourPM = Convert.ToInt32(s.Split(':')[0]);
+            char[] sss = s.ToCharArray();
+            string hour = "";
+            if (sss[8] == 'A')
+            {
+                if (hourPM == 12)
+                {
+                    hour = "00:" + s.Skip(3).Take(5);
+                }
+                else
+                {
+                    hour = s.Take(8).ToString();
+                }
+            }
+            else
+            {
+                if (hourPM != 12)
+                {
+                    hourPM += 12;
+                    hour = hourPM.ToString() + s.Skip(3).Take(5);
+                }
+                else
+                {
+                    hour = s.Take(8).ToString();
+                }
+            }
+            return hour;
         }
     }
 }
---
+++ HackerRankSolutions/QueensAttackII.cs
@@ -0,0 +1,233 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HackerRankSolutions
+{
+    public class QueensAttackII
+    {
+        /// <summary>
+        /// Asagidaki kod sonucu dogru veriyor ancak O(n^2) zamanda calistigi icin verimli degil. Commentlenen kodun altinda optimize edilmis metot blogu bulunmaktadir.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <param name="k"></param>
+        /// <param name="r_q"></param>
+        /// <param name="c_q"></param>
+        /// <param name="obstacles"></param>
+        /// <returns></returns>
+        //public int queensAttack(int n, int k, int r_q, int c_q, List<List<int>> obstacles)
+        //{
+        //    int count = 0;
+        //    for (int a = r_q - 1; a >= 1; a--)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == a && item[1] == c_q)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    for (int b = r_q + 1; b <= n; b++)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == b && item[1] == c_q)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    for (int c = c_q - 1; c >= 1; c--)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == r_q && item[1] == c)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    for (int d = c_q + 1; d <= n; d++)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == r_q && item[1] == c_q)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //    }
+        //    int row = r_q-1;
+        //    int column = c_q+1;
+        //    while (row<=n && column <=n)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == row && item[1] == column)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //        row--;
+        //        column++;
+        //    }
+        //    row = r_q + 1;
+        //    column = c_q - 1;
+        //    while (row <= n && column <= n)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == row && item[1] == column)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //        row++;
+        //        column--;
+        //    }
+        //    row = r_q - 1;
+        //    column = c_q - 1;
+        //    while (row <= n && column <= n)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == row && item[1] == column)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //        row--;
+        //        column--;
+        //    }
+        //    row = r_q + 1;
+        //    column = c_q + 1;
+        //    while (row <= n && column <= n)
+        //    {
+        //        int temp = 0;
+        //        foreach (var item in obstacles)
+        //        {
+        //            if (item[0] == row && item[1] == column)
+        //            {
+        //                temp++;
+        //                break;
+        //            }
+        //            else
+        //            {
+        //                count++;
+        //            }
+        //        }
+        //        if (temp > 0)
+        //        {
+        //            break;
+        //        }
+        //        row++;
+        //        column++;
+        //    }
+        //    return count;
+        //}
+        public int queensAttack(int n, int k, int r_q, int c_q, List<List<int>> obstacles)
+        {
+            int count = 0;
+            var obstacleSet = new HashSet<string>();
+
+            foreach (var obstacle in obstacles)
+            {
+                obstacleSet.Add($"{obstacle[0]}_{obstacle[1]}");
+            }
+
+            int[] dr = { -1, -1, -1, 0, 0, 1, 1, 1 };
+            int[] dc = { -1, 0, 1, -1, 1, -1, 0, 1 };
+
+            for (int i = 0; i < 8; i++)
+            {
+                int r = r_q + dr[i];
+                int c = c_q + dc[i];
+
+                while (r >= 1 && r <= n && c >= 1 && c <= n)
+                {
+                    if (obstacleSet.Contains($"{r}_{c}"))
+                    {
+                        break;
+                    }
+                    count++;
+                    r += dr[i];
+                    c += dc[i];
+                }
+            }
+            return count;
+        }
+    }
+}
